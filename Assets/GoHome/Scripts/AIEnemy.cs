602f6db4275d90b82eb406a5eb89a86717482608
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-02-27 15:16:57 +1100

    Initial commit
    Made in week 4 (27-02-2019)

---
+++ Assets/GoHome/Scripts/AIEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIEnemy : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent agent; //need to write the "using UnityEngine.AI" if I'm going to use any AI. Reference to the NavMeshAgent component
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update destination of NavMeshAgent
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/GoHome/Scripts/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+    //all variables needs to be weritten here...
+
+    // <Access-Specifier>   <Data-Type>     <Variable-Name> (How to write variables)
+         public             Transform        waypointParent; //transform because you want to move or transform the waypoint.
+    public float moveSpeed = 2f;
+    public float stoppingDistance = 1f;
+
+    public Transform[] waypoints;
+    private int currentIndex = 1;
+    private NavMeshAgent agent;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Get Children of waypointParent.
+        waypoints = waypointParent.GetComponentsInChildren<Transform>();
+        // Get reference to NavMeshAgent
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+
+
+    void Patrol()
+    {
+        // 1 - Get the current waypoint
+        Transform point = waypoints[currentIndex];
+        // 2 - Get distance to waypoint
+        float distance = Vector3.Distance(transform.position, point.position);
+        // 3 - If close to waypoint
+        if(distance < stoppingDistance)
+        {
+            //  4 - Switch to next waypoint
+            currentIndex++; // currentIndex = currentIndex + 1 / Adds 1 to number
+            if (currentIndex >= waypoints.Length)
+            {
+                currentIndex = 1;
+            }
+        }
+
+
+        // 5 - Translate enemy to waypoint
+        //transform.position = Vector3.MoveTowards(transform.position, point.position, moveSpeed * Time.deltaTime);
+
+        // 5 - Tell NavMeshAgent it's destination
+        agent.SetDestination(point.position);
+    }
+}
